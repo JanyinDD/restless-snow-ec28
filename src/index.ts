f096ae68d649413bb95b51b601da7df71984948f
Janyin_DD <94787524+JanyinDD@users.noreply.github.com>
2025-11-06 23:55:53 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "blocks world",
+      prompt: "minecraft,blocks world",
     };
 
     const response = await env.AI.run(
