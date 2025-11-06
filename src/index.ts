ce695fed76f12aa57cbbdf4e09012eb303858f00
Janyin_DD <94787524+JanyinDD@users.noreply.github.com>
2025-11-06 23:43:48 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "cute cat,small girl",
+      prompt: "cute cat,nude",
     };
 
     const response = await env.AI.run(
