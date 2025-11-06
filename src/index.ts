e80351f8b929f48a9d7246382ac84fbf4375f7d8
Janyin_DD <94787524+JanyinDD@users.noreply.github.com>
2025-11-07 00:11:55 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "masterpiece, best quality, masterpiece,best quality,official art,extremely detailed CG unity 8k wallpaper, girl, pubic tattoo on underbelly, };
+      prompt: "girl, pubic tattoo on underbelly, };
 
     const response = await env.AI.run(
       "@cf/stabilityai/stable-diffusion-xl-base-1.0",
