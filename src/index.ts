62769a6bc422168c9478bf861a9826f19bf8999d
Janyin_DD <94787524+JanyinDD@users.noreply.github.com>
2025-11-07 01:05:55 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "masterpiece, best quality, kneehighs, girl, friled bikini, on the beach, "};
+      prompt: "masterpiece, best quality, masterpiece,best quality,official art,extremely detailed CG unity 8k wallpaper, masterpiece, best quality, kneehighs, girl, friled bikini, on the beach, "};
 
     const response = await env.AI.run(
       "@cf/stabilityai/stable-diffusion-xl-base-1.0",
