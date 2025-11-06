8c91c4233ae918e396c3193084ec416febc1c737
Janyin_DD <94787524+JanyinDD@users.noreply.github.com>
2025-11-07 00:47:37 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "masterpiece, best quality, incredibly_absurdres, kneehighs, girl, artbook, friled bikini, on the beach,  "};
+      prompt: "masterpiece, best quality, kneehighs, girl, friled bikini, on the beach, "};
 
     const response = await env.AI.run(
       "@cf/stabilityai/stable-diffusion-xl-base-1.0",
