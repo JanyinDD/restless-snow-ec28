ae89f422ea8d929aed79cb5616ebc41d498ba01d
Janyin_DD <94787524+JanyinDD@users.noreply.github.com>
2025-11-06 23:41:12 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default {
   async fetch(request, env) {
     const inputs = {
-      prompt: "cyberpunk cat",
+      prompt: "cute cat,small girl",
     };
 
     const response = await env.AI.run(
